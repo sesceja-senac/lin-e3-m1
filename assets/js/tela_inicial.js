a470b6a5e1aa3c39ece9c5fbb2ae29e9bbea242f
Diegogete <ddrsilva@senacrs.com.br>
2023-01-10 06:48:14 -0300

    ajuste

--- assets/js/tela_inicial.js
+++ assets/js/tela_inicial.js
@@ -13,5 +13,26 @@ $(document).ready(function() {
       $('html').css('overflow', 'unset')
       localStorage.setItem('lstvideo-intro', 'true')
     })
+    
+    window.setTimeout(function(){
+      $('.aneis:nth-child(1)').removeClass("init");
+      console.log("teste");
+  }, 300)
+  window.setTimeout(function(){
+    $('.aneis:nth-child(2)').removeClass("init");
+    console.log("teste");
+}, 600)
+window.setTimeout(function(){
+  $('.aneis:nth-child(3)').removeClass("init");
+  console.log("teste");
+}, 900)
+window.setTimeout(function(){
+  $('.aneis:nth-child(4)').removeClass("init");
+  console.log("teste");
+}, 1200)
+window.setTimeout(function(){
+  $('.aneis:nth-child(5)').removeClass("init");
+  console.log("teste");
+}, 1500)
 });
   
---
+++ drag/README.md
@@ -0,0 +1,5 @@
+# drag-in-drop-template
+Template para criação de atividades ao estilo drag in drop. O usuário precisa arrastar itens para containers. 
+
+## Incompatibilidade com JQuery UI:
+O projeto é incompatível com o JQuery-UI. Caso ocorram problemas na validação do container ou o drag em dispositivos móveis, pode ser que o framework jquery-ui esteja lincado ao projeto. Basta remover o Jquery-UI do site que o Drag in Drop Template irá funcionar corretamente.
